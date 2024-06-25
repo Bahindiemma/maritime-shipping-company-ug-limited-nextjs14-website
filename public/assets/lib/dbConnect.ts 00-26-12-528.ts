b311a2a78a0585d0c1767cf20f778dacc3bd03c0
Bahindiemma <ebahindi@gmail.com>
2024-06-25 10:40:18 +0300

    Fix the db error

--- public/assets/lib/dbConnect.ts 00-26-12-528.ts
+++
@@ -1,9 +0,0 @@
-import _mongoose, { client } from "mongoose"
-
-declare global {
-    var mongoose: {
-        promise: ReturnType<typeof connect> | null;
-        conn: typeof _mongoose | null;
-    }
-}
-
